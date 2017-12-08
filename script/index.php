a74aa916ee6ea4ce86955f95739bada8b4ccc532
Aaron Lin <phoe721@yahoo.com>
2017-12-08 21:58:08 +0800

    Added gitignore file Fixed file path bug (put backward slash in front of spaces)

--- script/index.php
+++ script/index.php
@@ -5,6 +5,8 @@
 	// Main program
 	if (isset($_POST['video'])) {
 		$video = ROOT . urldecode($_POST['video']);
+		$video = str_replace(" ", "\ ", $video);
+		logger("debug: " . $video);
 		$screenshots = get_screenshots($video);
 		echo json_encode($screenshots);
 	}
